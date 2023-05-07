e128f55d2d0bac210382fc47b7e4df86a3066101
cedylad <cedricanassi@gmail.com>
2023-05-07 13:13:17 +0200

    Génération des célébrités et activités aléatoirement

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -8,9 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var celebrities = ["le Steve Jobs", "le Zinedine Zidane", "la Madonna", "le Karl Lagerfeld", "la Scarlett Johansson"]
+    
+    var activities = ["du dancefloor", "du barbecue", "de la surprise ratée", "des blagues lourdes", "de la raclette party"]
+    
+    
     @IBOutlet weak var quoteLabel: UILabel!
     
     @IBAction func changeQuote(_ sender: Any) {
+        
+        //Partie celebrities
+        var randomIndexCelebrity = Int.random(in: 0..<celebrities.count)
+        var randomCelebrety = celebrities[randomIndexCelebrity]
+        print(randomCelebrety)
+        
+        //Partie activities
+        var randomIndexActivity = Int.random(in: 0..<celebrities.count)
+        var randomActivity = activities[randomIndexCelebrity]
+        print(randomActivity)
+        
+        quoteLabel.text = "Le texte du label a été modifié !"
     }
     
     override func viewDidLoad() {
