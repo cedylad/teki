79e27cd723e06bedbffaaa0127cc5d9345bc9a7a
cedylad <cedricanassi@gmail.com>
2023-05-07 11:59:26 +0200

    Decouverte d'outlet et de view controller (variables et fonction

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    @IBAction func changeQuote(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
